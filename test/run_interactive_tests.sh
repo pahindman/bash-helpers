8165eb7e91a9ffdba3b11815c39d3fb464660d85
Phil Hindman <pahindman@user.noreply.github.com>
2026-09-06 16:33:10 -0500

    Rename run_tests.sh to run_interactive_tests.sh
    - don't run bats tests
    - avoid shellcheck warnings (by removing 'cd' command)

--- test/run_interactive_tests.sh
+++ test/run_interactive_tests.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
 BASH_SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PATH=$BASH_SOURCE_DIR/..:$PATH
-cd $BASH_SOURCE_DIR
-unset BASH_SOURCE_DIR
 
-bats . --print-output-on-failure --pretty
 read -n 1 -s -r -p "######## Press key to run use_terminal ########"
 echo
-./use_terminal
+"$BASH_SOURCE_DIR"/use_terminal
 read -n 1 -s -r -p "######## Press key to run use_progress_bar ########"
 echo
-./use_progress_bar
+"$BASH_SOURCE_DIR"/use_progress_bar
 read -n 1 -s -r -p "######## Press key to run use_spinner ########"
 echo
-./use_spinner
+"$BASH_SOURCE_DIR"/use_spinner
 read -n 1 -s -r -p "######## Press key to run use_spinner_with_progress_bar ########"
 echo
-./use_spinner_with_progress_bar
+"$BASH_SOURCE_DIR"/use_spinner_with_progress_bar
